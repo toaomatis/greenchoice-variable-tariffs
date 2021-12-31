67fd7af006a0ab75cfc68df1baacf6bac74fe283
Mathijs de Kruyf <mathijs.de.kruyf@audax.nl>
2021-12-31 13:54:01 +0100

    Fix test

--- tests/test_greenchoice_api.py
+++ tests/test_greenchoice_api.py
@@ -1,8 +1,8 @@
 """Test component setup."""
 import json
-import os
 
 from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import load_fixture
 from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 
 from custom_components.greenchoice_variable_tariffs.const import (
@@ -17,12 +17,12 @@ from tests.const import MOCK_CONFIG
 
 async def test_greenchoice_api(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, skip_setup: bool = False, ):
     """Test Greenchoice API."""
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(f'{dir_path}/fixtures/01_response.json') as f:
-        response_dict = json.load(f)
+    response_dict = json.loads(load_fixture('01_response.json'))
 
-    aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation', json=response_dict)
-    api = GreenchoiceApiData(**MOCK_CONFIG)
+    aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation',
+                       json=response_dict,
+                       headers={"Content-Type": "application/json"}, )
+    api = GreenchoiceApiData(hass, **MOCK_CONFIG)
 
     await api.async_update()
     result = api.result
