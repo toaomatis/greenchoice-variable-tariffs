7daee9e92910b206ea8dfbaebad0140d96c98d2a
Mathijs de Kruyf <mathijs.de.kruyf@audax.nl>
2021-12-31 14:16:08 +0100

    Update tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,9 @@
 """Test component setup."""
+from homeassistant.core import HomeAssistant
+
 from custom_components.greenchoice_variable_tariffs import async_setup
 
 
-async def test_async_setup(hass):
+async def test_async_setup(hass: HomeAssistant):
     """Test the component gets setup."""
     assert await async_setup(hass, {}) is True
