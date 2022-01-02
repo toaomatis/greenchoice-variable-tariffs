1261652e060d2600129bc1b5edb96b91b1a43a45
Mathijs de Kruyf <mathijs.de.kruyf@audax.nl>
2022-01-02 18:09:03 +0100

    Update GreenchoiceApiTest

--- tests/test_greenchoice_api.py
+++ tests/test_greenchoice_api.py
@@ -12,17 +12,21 @@ from custom_components.greenchoice_variable_tariffs.const import (
     SENSOR_TYPE_NORMAL_TARIFF
 )
 from custom_components.greenchoice_variable_tariffs.sensor import GreenchoiceApiData
-from tests.const import MOCK_CONFIG
+from tests.const import (
+    MOCK_CONFIG_ALL, 
+    MOCK_CONFIG_ELECTRICITY,
+    MOCK_CONFIG_ELECTRICITY_NORMAL
+)
 
 
-async def test_greenchoice_api(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, skip_setup: bool = False, ):
-    """Test Greenchoice API."""
+async def test_greenchoice_api_all(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, skip_setup: bool = False, ):
+    """Test Greenchoice API for All sensors."""
     response_dict = json.loads(load_fixture('01_response.json'))
 
     aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation',
                        json=response_dict,
                        headers={"Content-Type": "application/json"}, )
-    api = GreenchoiceApiData(hass, **MOCK_CONFIG)
+    api = GreenchoiceApiData(hass, **MOCK_CONFIG_ALL)
 
     await api.async_update()
     result = api.result
@@ -33,3 +37,40 @@ async def test_greenchoice_api(hass: HomeAssistant, aioclient_mock: AiohttpClien
     assert result[SENSOR_TYPE_NORMAL_TARIFF] == 0.5367
     assert result[SENSOR_TYPE_LOW_TARIFF] == 0.4136
     assert result[SENSOR_TYPE_GAS_TARIFF] == 2.0485
+
+
+async def test_greenchoice_api_electricity(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, skip_setup: bool = False, ):
+    """Test Greenchoice API for Electricity sensors."""
+    response_dict = json.loads(load_fixture('01_response.json'))
+
+    aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation',
+                       json=response_dict,
+                       headers={"Content-Type": "application/json"}, )
+    api = GreenchoiceApiData(hass, **MOCK_CONFIG_ELECTRICITY)
+
+    await api.async_update()
+    result = api.result
+    assert SENSOR_TYPE_NORMAL_TARIFF in result
+    assert SENSOR_TYPE_LOW_TARIFF in result
+    assert SENSOR_TYPE_GAS_TARIFF not in result
+    assert SENSOR_MEASUREMENT_DATE in result
+    assert result[SENSOR_TYPE_NORMAL_TARIFF] == 0.5367
+    assert result[SENSOR_TYPE_LOW_TARIFF] == 0.4136
+
+
+async def test_greenchoice_api_electricity_normal(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker, skip_setup: bool = False, ):
+    """Test Greenchoice API for Electricity sensors."""
+    response_dict = json.loads(load_fixture('01_response.json'))
+
+    aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation',
+                       json=response_dict,
+                       headers={"Content-Type": "application/json"}, )
+    api = GreenchoiceApiData(hass, **MOCK_CONFIG_ELECTRICITY_NORMAL)
+
+    await api.async_update()
+    result = api.result
+    assert SENSOR_TYPE_NORMAL_TARIFF in result
+    assert SENSOR_TYPE_LOW_TARIFF not in result
+    assert SENSOR_TYPE_GAS_TARIFF not in result
+    assert SENSOR_MEASUREMENT_DATE in result
+    assert result[SENSOR_TYPE_NORMAL_TARIFF] == 0.5367
--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -3,12 +3,12 @@ from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.greenchoice_variable_tariffs.const import DOMAIN
-from tests.const import MOCK_CONFIG
+from tests.const import MOCK_CONFIG_ALL
 
 
 async def test_async_setup(hass: HomeAssistant):
     """Test the component gets setup."""
-    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG)
+    entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_ALL)
     entry.add_to_hass(hass)
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
