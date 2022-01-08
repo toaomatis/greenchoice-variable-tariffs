b8a717a9769544cdcfb2048f3fda53ec43542ec4
Mathijs de Kruyf <mathijs.de.kruyf@audax.nl>
2022-01-08 14:38:51 +0100

    Before rework

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,47 @@
 """Tests for the Greenchoice Variable Tariffs Sensor integration."""
+
+import json
+from unittest.mock import PropertyMock, patch
+from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture, patch_yaml_files
+
+from custom_components.greenchoice_variable_tariffs.const import DOMAIN
+from tests.const import MOCK_CONFIG_ALL
+
+
+async def init_integration(
+    hass, forecast=False, unsupported_icon=False
+) -> MockConfigEntry:
+    """Set up the AccuWeather integration in Home Assistant."""
+    options = {}
+    if forecast:
+        options["forecast"] = True
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        title="Home",
+        unique_id="0123456",
+        data=MOCK_CONFIG_ALL,
+        options=options,
+    )
+
+    current = json.loads(load_fixture('01_response.json'))
+
+    if unsupported_icon:
+        current["WeatherIcon"] = 999
+
+    # with patch(
+    #     "homeassistant.components.greenchoice_variable_tariffs.GreenchoiceApiData.async_update",
+    #     return_value=current,
+    # ), patch(
+    #     "homeassistant.components.greenchoice_variable_tariffs.AccuWeather.async_get_forecast",
+    #     return_value=forecast,
+    # ), patch(
+    #     "homeassistant.components.greenchoice_variable_tariffs.AccuWeather.requests_remaining",
+    #     new_callable=PropertyMock,
+    #     return_value=10,
+    # ):
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    return entry
--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,15 +1,31 @@
 """Test component setup."""
+import logging
+import json
 from homeassistant.core import HomeAssistant
-from pytest_homeassistant_custom_component.common import MockConfigEntry
+from homeassistant.helpers import entity_registry as er
+from pytest_homeassistant_custom_component.common import MockConfigEntry, load_fixture
+from pytest_homeassistant_custom_component.test_util.aiohttp import AiohttpClientMocker
 
 from custom_components.greenchoice_variable_tariffs.const import DOMAIN
 from tests.const import MOCK_CONFIG_ALL
 
+_LOGGER = logging.getLogger(__name__)
 
-async def test_async_setup(hass: HomeAssistant):
-    """Test the component gets setup."""
+
+async def test_async_setup(hass: HomeAssistant, aioclient_mock: AiohttpClientMocker,):
+    response_dict = json.loads(load_fixture('01_response.json'))
+
+    aioclient_mock.get('https://www.greenchoice.nl/umbraco/surface/quotation/GetQuotation',
+                       json=response_dict,
+                       headers={"Content-Type": "application/json"}, )
+    
     entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG_ALL)
     entry.add_to_hass(hass)
     await hass.config_entries.async_setup(entry.entry_id)
     await hass.async_block_till_done()
-    assert True
+
+    all_states = hass.states.get(entry.entry_id)
+    _LOGGER.warn(f'{all_states=}')
+
+    state = hass.states.get("sensor.greenchoice_electricity_normal_tariff")
+    assert state
