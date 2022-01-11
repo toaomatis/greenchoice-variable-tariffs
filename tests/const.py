5c18f2ef3293c53cf8dcd69eaf148d628f098ab8
Mathijs <toaomatis@users.noreply.github.com>
2022-01-11 18:51:38 +0100

    Increase coverage (#10)
    * Added friendly name (#9)
    
    Remove unused fields
    
    * Update GreenchoiceApiTest
    
    * Fully test GreenchoiceApiData
    
    * Remove unused parameters
    
    * Before rework
    
    * Disable test
    
    * Add exposed sensors

--- tests/const.py
+++ tests/const.py
@@ -7,9 +7,37 @@ from custom_components.greenchoice_variable_tariffs.const import (
 )
 
 # Mock config data to be used across multiple tests
-MOCK_CONFIG = {
+MOCK_CONFIG_ALL = {
     CONF_POSTAL_CODE: "9999ZZ",
     CONF_USE_GAS: True,
     CONF_USE_LOW_TARIFF: True,
     CONF_USE_ELECTRICITY: True,
+}
+
+MOCK_CONFIG_ELECTRICITY = {
+    CONF_POSTAL_CODE: "9999ZZ",
+    CONF_USE_GAS: False,
+    CONF_USE_LOW_TARIFF: True,
+    CONF_USE_ELECTRICITY: True,
+}
+
+MOCK_CONFIG_ELECTRICITY_NORMAL = {
+    CONF_POSTAL_CODE: "9999ZZ",
+    CONF_USE_GAS: False,
+    CONF_USE_LOW_TARIFF: False,
+    CONF_USE_ELECTRICITY: True,
+}
+
+MOCK_CONFIG_GAS = {
+    CONF_POSTAL_CODE: "9999ZZ",
+    CONF_USE_GAS: True,
+    CONF_USE_LOW_TARIFF: False,
+    CONF_USE_ELECTRICITY: False,
+}
+
+MOCK_CONFIG_NONE = {
+    CONF_POSTAL_CODE: "9999ZZ",
+    CONF_USE_GAS: False,
+    CONF_USE_LOW_TARIFF: False,
+    CONF_USE_ELECTRICITY: False,
 }
